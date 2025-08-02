75a38dfdea5feebe0511501f074ab61ac59e7991
unreal7369 <shivam23p25@gmail.com>
2025-08-02 16:48:32 +0000

    Added Valentine app code and requirements

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,21 @@
 import streamlit as st
+from PIL import Image
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.set_page_config(layout="centered", page_title="Love Me Valentine")
+st.markdown("## ❤️ **Love Me Valentine**")
+
+uploaded = st.file_uploader("Choose an image", type=["png", "jpg", "jpeg"])
+if uploaded:
+    st.image(Image.open(uploaded), width=200)
+else:
+    st.info("Upload any picture (it doesn't have to be named valentine.png)")
+
+st.markdown("### Do you love me?")
+col1, col2 = st.columns(2)
+response = ""
+if col1.button("No"):
+    response = "😢 Oh... I’ll keep trying!"
+if col2.button("Yes"):
+    response = "🥰 I love you too!"
+if response:
+    st.success(response)
